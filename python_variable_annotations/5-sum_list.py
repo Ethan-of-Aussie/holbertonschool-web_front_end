8ff64d22af0ba8e8f1c50f4c8aedc9bb9c50d5dd
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-07-31 14:11:43 +1000

    5-sum_list

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Module defines sum_list"""
 
+from typing import List
 
-def sum_list(input_list: typing.List[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Returns sums as a float"""
     return sum(input_list)
