fb8a47d5b4f3074d25263738ba2e09fc47fcb187
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-08-11 08:15:40 +1000

    0-async_generator.py

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
+"""Defines function async generator"""
 
 
 import random, asyncio
 
-
 async def async_generator():
+    """Between 0 to 10, Numbers are generated and waited per loop"""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
-    
