dbbc2ed7f53116c2616a5b489a2cd7ebf07a3e93
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-08-10 21:22:33 +1000

    3-tasks.py

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Module that define task_wait_random as non-async function"""
 
+
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-def task_wait_random(max_delay):
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """None async function that takes a int and returns asyncio.Task"""
     return asyncio.Task(wait_random(max_delay))
